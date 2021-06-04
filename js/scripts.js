85eac906f39f03db0b1a90be74d3d0d04f78410e
smita Raj <smita.raj12@gmail.com>
2021-06-04 16:41:28 -0700

    Bug Fix : trying to fix bug in script

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     const qulInput = $("#qul").val();
     const ageInput = parseInt($("#Age").val());
     const operator = $("input:radio[name=operator]:checked").val();
+    if(nameInput === 'name' && qulInput === 'qul' && ageInput >= 15){
       if (operator === "hot") {
         $("#python").show()
       }else if (operator === "less") {
@@ -16,25 +17,21 @@ $(document).ready(function() {
       }else if(operator === "timedue"){
         $("#Swift").show()
       }
-      const veryhot = $("select#vhot").val();
-
-      $("#check").submit(function(event) {
-        $("#check").hide();
-        if(veryhot == 'vhot'){
-          alert('you can select python')
-        }
-    });
-    event.preventDefault();
+    }else{
+      $("#age").show()
+    }  
+      event.preventDefault();
   });
-  
+  });
+  $(document).ready(function() {
   
       $("#check").submit(function(event) {
         $("#check").hide();
-        const Iname = $("#uname")
+        const Iname = $("#uname").val()
         const veryhot = $("select#vhot").val();
         if(veryhot === 'vhot' && Iname === 'uname' ){
           $("#vhotmsg").show()
         }
     });
-});
+  });
 
